204a3d8a9beb7e5bdaac68653e4054cba8f542f3
Roberto González Michelena <robemadz@gmail.com>
2023-05-12 17:51:11 +0200

    añadidos blog posts con graphql y posts mdx de prueba

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog/`,
+      },
+    },
     `gatsby-plugin-postcss`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
