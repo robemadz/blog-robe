773aa12f885bdc8495de805348fef32cf4d398d1
Roberto González Michelena <robemadz@gmail.com>
2023-05-12 13:34:10 +0200

    añadidos estilos, creada página de blog

--- src/components/header.js
+++ src/components/header.js
@@ -3,22 +3,28 @@ import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 const Header = () => (
-  <header className="bg-pink-500">
+  <header className="bg-zinc-100 border border-b-2 border-b-zinc-300">
     <nav className="flex justify-between items-center max-w-screen-2xl mx-auto py-4">
-      <Link to="/">
+      <Link className="bg-pink-500/90 rounded-full" to="/">
         <StaticImage
+          className="relative -top-1 -left-1"
           alt="basketball outlined icon"
           src="../images/basketball-icon.png"
         />
       </Link>
       <ul className="flex font-medium">
         <li className="mx-2">
-          <Link className="block px-6 py-2 hover:bg-emerald-500" to="/">
+          <Link className="block px-6 py-2 hover:bg-pink-500/90" to="/">
             Home
           </Link>
         </li>
         <li className="mx-2">
-          <Link className="block px-6 py-2 hover:bg-emerald-500" to="/about">
+          <Link className="block px-6 py-2 hover:bg-pink-500/90" to="/blog">
+            Blog
+          </Link>
+        </li>
+        <li className="mx-2">
+          <Link className="block px-6 py-2 hover:bg-pink-500/90" to="/about">
             About
           </Link>
         </li>
