73d49980f186d926501ce4eb6d6704c7f001e87e
Roberto González Michelena <robemadz@gmail.com>
2023-05-14 14:01:52 +0200

    renderizado dinámico de thumbnails en las cards de cada blogpost. Maquetado de tarjetas

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,34 +1,50 @@
 import * as React from "react"
 import { Link, graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import Layout from "../../components/layout"
 import Seo from "../../components/seo"
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <section className="max-w-screen-2xl mx-auto flex flex-col justify-between md:flex-row gap-4 mt-20">
-        {data.allMdx.nodes.map(node => (
-          <article
-            className="relative bg-white py-4 px-5 border border-zinc-100 shadow-sm mx-4 hover:-translate-y-1 hover:-translate-x-1 hover:shadow-md transition"
-            key={node.id}
-          >
-            <h2 className="text-3xl font-semibold text-zinc-700 mb-2">
-              {node.frontmatter.title}
-            </h2>
-            <p className="text-sm text-zinc-500 pb-1 border-b border-b-zinc-300 mb-3">
-              Publicado: {node.frontmatter.date}
-            </p>
-            <p className="mb-6">{node.frontmatter.description}</p>
-            <div className="text-right">
-              <Link
-                className="mr-auto font-medium text-zinc-700 hover:text-zinc-400 transition"
-                to={`/blog/${node.frontmatter.slug}`}
-              >
-                Leer artículo →
-              </Link>
-            </div>
-          </article>
-        ))}
+      <section className="max-w-screen-xl mx-auto flex justify-center flex-wrap gap-4 mt-20">
+        {data.allMdx.nodes.map(node => {
+          const images = node.frontmatter.hero_image.childrenImageSharp.map(
+            image => getImage(image.gatsbyImageData)
+          )
+          return (
+            <article
+              className="relative max-w-[400px] bg-white py-4 px-5 border border-zinc-100 shadow-sm mx-2 hover:-translate-y-1 hover:-translate-x-1 hover:shadow-md transition"
+              key={node.id}
+            >
+              <h2 className="text-3xl font-semibold text-zinc-700 mb-3 line-clamp-2 h-[72px]">
+                {node.frontmatter.title}
+              </h2>
+              <p className="text-sm text-zinc-500 pb-1 border-b border-b-zinc-300 mb-3">
+                Publicado: {node.frontmatter.date}
+              </p>
+              <div>
+                {images.map(image => (
+                  <GatsbyImage
+                    className="my-8"
+                    key={node.id}
+                    image={image}
+                    alt={node.frontmatter.hero_image_alt}
+                  />
+                ))}
+              </div>
+              <p className="mb-6">{node.frontmatter.description}</p>
+              <div className="text-right">
+                <Link
+                  className="mr-auto font-medium text-zinc-700 hover:text-zinc-400 transition"
+                  to={`/blog/${node.frontmatter.slug}`}
+                >
+                  Leer artículo →
+                </Link>
+              </div>
+            </article>
+          )
+        })}
       </section>
     </Layout>
   )
@@ -43,6 +59,12 @@ export const query = graphql`
           slug
           title
           description
+          hero_image_alt
+          hero_image {
+            childrenImageSharp {
+              gatsbyImageData
+            }
+          }
         }
         id
       }
